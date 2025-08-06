7b9cd7db07bf2a77d64de0c936c9ae5e21865d03
Matias Petenatti <matiasodin96@gmail.com>
2025-08-06 12:01:13 -0300

    feat: exercise 1.9

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -5,8 +5,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -16,7 +14,6 @@ const App = () => {
         <button onClick={() => setBad(bad + 1)}>bad</button>
       </div>
       <Statistics good={good} neutral={neutral} bad={bad} />
-
     </div>
   )
 }
@@ -25,15 +22,19 @@ export default App
 
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
-  return (
-    <>
-      <h2>Statistics</h2>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {total}</p>
-      <p>average: {(good - bad) / total}</p>
-      <p>positive: {(good / total) * 100} %</p>
-    </>
-  )
+  if (total == 0) {
+    return <p>No feedback given</p>
+  } else {
+    return (
+      <>
+        <h2>Statistics</h2>
+        <p>good: {good}</p>
+        <p>neutral: {neutral}</p>
+        <p>bad: {bad}</p>
+        <p>all: {total}</p>
+        <p>average: {(good - bad) / total}</p>
+        <p>positive: {(good / total) * 100} %</p>
+      </>
+    )
+  }
 }
